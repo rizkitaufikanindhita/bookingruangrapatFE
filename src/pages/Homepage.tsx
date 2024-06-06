2046ba587c386272683bc2258d1a7c1e0d127a9e
Rizki Taufik Anindhita <rizki.anindhita@binus.ac.id>
2024-06-07 00:55:33 +0700

    fix footer

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -6,15 +6,17 @@ import MenuHomapageComp from "@/components/MenuHomepageComp"
 const Homepage = () => {
   const navigate = useNavigate()
   const token = localStorage.getItem("token")
-  if(token){
+  if (token) {
     navigate("/dashboard")
   }
   return (
-  <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-      <MenuHomapageComp />
-      <HomepageComp />
+    <div className="flex flex-col min-h-screen absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <div className="flex-grow">
+        <MenuHomapageComp />
+        <HomepageComp />
+      </div>
       <FooterComp />
-  </div>
+    </div>
   )
 }
 
