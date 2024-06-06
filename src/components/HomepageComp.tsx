af04b1db4d243e3fd88ac9b31c1c3085be24227c
Rizki Taufik Anindhita <rizki.anindhita@binus.ac.id>
2024-06-07 00:17:43 +0700

    add

--- src/components/HomepageComp.tsx
+++ src/components/HomepageComp.tsx
@@ -1,12 +1,9 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
 import { Separator } from "@/components/ui/separator"
 const url = import.meta.env.VITE_URL_GET
-const urlDelete = import.meta.env.VITE_URL_DELETE
 
 const DashboardComp = () => {
-  const navigate = useNavigate()
   type clockType = {
     hours: number,
     minutes: number
@@ -73,12 +70,6 @@ const DashboardComp = () => {
     return daysMap[day.toLowerCase()]; // Mengembalikan "minggu" jika tidak ada pasangan kunci yang cocok
   };
 
-  const deleteTask = async (e: any) => {
-    await axiosWithToken.delete(`${urlDelete}/${e}`)
-    alert("booking berhasil dihapus")
-    window.location.reload()
-  }
-
   return (
     <div>
       <div className="flex justify-center items-center">
@@ -103,19 +94,6 @@ const DashboardComp = () => {
                   {meetingsForDay.length > 0 ? (
                     meetingsForDay.map((book, index) => (
                       <div key={index} className="border p-3 rounded-lg bg-gradient-to-br from-slate-400 to-slate-300 text-slate-950 shadow-[rgba(13,_38,_76,_0.19)_0px_5px_7px]">
-                        <div className="flex justify-end ml-24 scale-75">
-                          {/* update */}
-                          <button className="mx-3" onClick={() => navigate(`/edit?id=${book._id}`)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                          </svg>
-                          </button>
-                          {/* delete */}
-                          <button onClick={() => deleteTask(book._id)}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                          </svg>
-                          </button>
-                        </div>
-                        {/* <div>{getDayFromDate(book.day.toString())}</div> */}
                         <div className="font-bold text-xl">{formatDate(parseInt(book.date.toString()))}                    </div>
                         <Separator className="my-3" />
                         <div className="font-bold">{book.event.toUpperCase()}</div>
