0f8d2c34b7f72890d75018c710c81cdeca8ad32c
Rizki Taufik Anindhita <rizki.anindhita@binus.ac.id>
2024-06-12 14:08:43 +0700

    add pwa

--- src/components/MenuHomepageComp.tsx
+++ src/components/MenuHomepageComp.tsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "@/components/ui/button"
+
 const MenuHomapageComp = () => {
+  const navigate = useNavigate()
+
   return (
-    <div className="bg-white shadow-[rgba(13,_38,_76,_0.19)_0px_5px_10px] flex justify-center items-center">
+    <div className="bg-white shadow-[rgba(13,_38,_76,_0.19)_0px_5px_10px] flex justify-between items-center">
       <div className="py-4 px-7 font-bold text-xl md:text-3xl text-center">
         Agenda Rapat
       </div>
+      <Button className="mx-7" onClick={()=>navigate("/signin")}>Sign in</Button>
     </div>
   )
 }
