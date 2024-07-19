e098ea26f4afd093b2ea40ab44c10ba294bff7e5
rizkitaufikanindhita <rizki.anindhita@binus.ac.id>
2024-07-19 15:05:02 +0700

    update homepage

--- src/components/HomepageComp.tsx
+++ src/components/HomepageComp.tsx
@@ -1,42 +1,44 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Separator } from "@/components/ui/separator"
-import { useNavigate } from "react-router-dom"
-const url = import.meta.env.VITE_URL_GET
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Separator } from "@/components/ui/separator";
+import { useNavigate } from "react-router-dom";
+const url = import.meta.env.VITE_URL_GET;
 
 const DashboardComp = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   type clockType = {
-    hours: number,
-    minutes: number
-  }
+    hours: number;
+    minutes: number;
+  };
 
   type dataType = {
-    _id: String,
-    day: String,
-    date: String,
-    event: String,
-    clockStart: clockType,
-    clockEnd: clockType,
-    room: String,
-    pic: String,
-    kapasitas: String
-  }
-  const [booking, setBooking] = useState<dataType[]>([])
+    _id: String;
+    day: String;
+    date: String;
+    event: String;
+    clockStart: clockType;
+    clockEnd: clockType;
+    room: String;
+    pic: String;
+    kapasitas: String;
+    rapat: String;
+    catatan: String;
+  };
+  const [booking, setBooking] = useState<dataType[]>([]);
 
   const axiosWithToken = axios.create({
     headers: {
       Authorization: "Bearer " + localStorage.getItem("token"),
-      "Content-Type": "application/json"
-    }
-  })
+      "Content-Type": "application/json",
+    },
+  });
 
   const fetchData = async () => {
-    const response = await axiosWithToken.get(url)
-    setBooking(response.data.msg)
-  }
+    const response = await axiosWithToken.get(url);
+    setBooking(response.data.msg);
+  };
 
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
@@ -45,9 +47,7 @@ const DashboardComp = () => {
   }, []);
 
   if (loading == true) {
-    return (
-      <div>Loading...</div>
-    )
+    return <div>Loading...</div>;
   }
 
   const formatDate = (epochMillis: number) => {
@@ -57,36 +57,38 @@ const DashboardComp = () => {
 
   const formatTime = (clock: clockType) => {
     const formattedHours = clock.hours < 10 ? `0${clock.hours}` : clock.hours;
-    const formattedMinutes = clock.minutes === 0 ? `${clock.minutes}0` : clock.minutes;
+    const formattedMinutes =
+      clock.minutes === 0 ? `${clock.minutes}0` : clock.minutes;
     return `${formattedHours}:${formattedMinutes}`;
   };
 
   const getDayFromDate = (day: string) => {
     const daysMap: { [key: string]: string } = {
-      "monday": "Senin",
-      "tuesday": "Selasa",
-      "wednesday": "Rabu",
-      "thursday": "Kamis",
-      "friday": "Jumat",
-      "saturday": "Sabtu",
-      "sunday": "Minggu"
+      monday: "Senin",
+      tuesday: "Selasa",
+      wednesday: "Rabu",
+      thursday: "Kamis",
+      friday: "Jumat",
+      saturday: "Sabtu",
+      sunday: "Minggu",
     };
     return daysMap[day.toLowerCase()]; // Mengembalikan "minggu" jika tidak ada pasangan kunci yang cocok
   };
 
-  if(localStorage.getItem("token")){
-    navigate("/dashboard")
+  if (localStorage.getItem("token")) {
+    navigate("/dashboard");
   }
 
   return (
     <div>
       <div className="md:flex md:justify-center items-center">
         <div className="md:flex space-x-1 md:space-x-4 p-5">
-          {['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'].map((hari) => {
+          {["Senin", "Selasa", "Rabu", "Kamis", "Jumat"].map((hari) => {
             const meetingsForDay = booking
               .filter((book) => getDayFromDate(book.day.toString()) === hari)
               .sort((a, b) => {
-                const dateComparison = parseInt(a.date.toString()) - parseInt(b.date.toString())
+                const dateComparison =
+                  parseInt(a.date.toString()) - parseInt(b.date.toString());
                 if (dateComparison !== 0) return dateComparison;
 
                 if (a.clockStart.hours !== b.clockStart.hours) {
@@ -96,19 +98,34 @@ const DashboardComp = () => {
               });
             // benerin lagi urutannya
             return (
-              <div key={hari} className="flex flex-col border p-5 rounded-lg mt-3 w-full md:w-56 bg-white shadow-[rgba(13,_38,_76,_0.19)_0px_9px_10px]">
+              <div
+                key={hari}
+                className="flex flex-col border p-5 rounded-lg mt-3 w-full md:w-56 bg-white shadow-[rgba(13,_38,_76,_0.19)_0px_9px_10px]"
+              >
                 <div className="text-center font-bold text-2xl">{hari}</div>
                 <div className="flex flex-col space-y-3 mt-3">
                   {meetingsForDay.length > 0 ? (
                     meetingsForDay.map((book, index) => (
-                      <div key={index} className="border p-3 rounded-lg bg-gradient-to-br from-slate-400 to-slate-300 text-slate-950 shadow-[rgba(13,_38,_76,_0.19)_0px_5px_7px]">
-                        <div className="font-bold text-xl">{formatDate(parseInt(book.date.toString()))}                    </div>
+                      <div
+                        key={index}
+                        className="border p-3 rounded-lg bg-gradient-to-br from-slate-400 to-slate-300 text-slate-950 shadow-[rgba(13,_38,_76,_0.19)_0px_5px_7px]"
+                      >
+                        <div className="font-bold text-xl">
+                          {formatDate(parseInt(book.date.toString()))}{" "}
+                        </div>
                         <Separator className="my-3" />
-                        <div className="font-bold">{book.event.toUpperCase()}</div>
-                        <div>{formatTime(book.clockStart)} - {formatTime(book.clockEnd)}</div>
+                        <div className="font-bold">
+                          {book.event.toUpperCase()}
+                        </div>
+                        <div>
+                          {formatTime(book.clockStart)} -{" "}
+                          {formatTime(book.clockEnd)}
+                        </div>
                         <div>{book.room}</div>
                         <div>PIC: {book.pic}</div>
                         <div>Kapasitas: {book.kapasitas}</div>
+                        <div>Rapat {book.rapat}</div>
+                        <div>Catatan: {book.catatan}</div>
                       </div>
                     ))
                   ) : (
@@ -121,8 +138,7 @@ const DashboardComp = () => {
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default DashboardComp 
+export default DashboardComp;
